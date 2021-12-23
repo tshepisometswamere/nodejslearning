c58941193d1e7916e5e49ce9af47b3835e5ad1bf
TSHEPISO METSWAMERE <tshepiso.metswamere@mtn.com>
2021-12-23 10:48:38 +0200

    feat: playing with nodejs

---
+++ play/play.js
@@ -0,0 +1,12 @@
+const name = 'Tshepiso';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+
+const summarizeUser = function(userName,userAge,userHasHobbies) {
+
+    return 'Name is ' + userName + ', and age is ' + userAge + ', and user has HHobbies' + userHasHobbies;
+}
+
+console.log(summarizeUser(name,age,hasHobbies));
